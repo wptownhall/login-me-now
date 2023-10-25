538c9ff1619e74e7000d32a2c8fd4f4891e89e6c
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-10-25 11:43:55 +0600

    temporary login text size, box padding adjustment and image added

--- App/Admin/Assets/src/dashboard-app/pages/components/SubTitle.js
+++ App/Admin/Assets/src/dashboard-app/pages/components/SubTitle.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { __ } from "@wordpress/i18n";
 
-function SubTitle({text, size}) {
+function SubTitle({text, size, pr}) {
   return (
-    <p className={`text-base leading-[1.625rem] text-slate-600 pb-7 ${size}`}>
+    <p className={`text-base leading-[1.625rem] text-slate-600 pb-7 ${pr} ${size}`}>
       {__(
         `${text}`,
         "login-me-now"
