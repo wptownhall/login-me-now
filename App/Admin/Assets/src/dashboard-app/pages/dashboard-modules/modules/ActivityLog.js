8e99f047a053eaa55bf44769d9727bad04cc8fab
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-04 10:58:15 +0600

    simple history module is done

--- App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/modules/ActivityLog.js
+++ App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/modules/ActivityLog.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import apiFetch from "@wordpress/api-fetch";
 import { __ } from "@wordpress/i18n";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import {Tooltip} from "antd";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -21,16 +22,13 @@ function ActivityLog({ colorChange, proItem, isAvailable }) {
   };
 
   const enableDmActivityLog = useSelector((state) => state.dmActivityLog);
-  console.log(enableDmActivityLog)
   const handleDmActivityLog = () => {
     let assetStatus;
     if (enableDmActivityLog === false || enableDmActivityLog === undefined) {
       assetStatus = true;
-    } else {
+    } else{
       assetStatus = false;
     }
-
-    console.log(assetStatus)
 
     dispatch({
       type: "ENABLE_DM_ACTIVITY_LOGS",
@@ -112,11 +110,12 @@ function ActivityLog({ colorChange, proItem, isAvailable }) {
               >
                 Settings
               </button>
+              <Tooltip placement="left" title={`${lmn_admin.simple_history_status === '1' ? '' : 'Please activate Simple History plugin to use this module'}`}>
               <Switch
-                onChange={lmn_admin.simple_history_status === 1 ? handleDmActivityLog : null}
+                onChange={lmn_admin.simple_history_status === '1' ? handleDmActivityLog : null}
                 className={classNames(
-                  enableDmActivityLog ? "bg-lmn" : "bg-slate-200",
-                  "group relative inline-flex h-[8px] w-[32px] flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-lmn focus:ring-offset-2"
+                  (lmn_admin.simple_history_status === '1' ? enableDmActivityLog : false) ? "bg-lmn" : "bg-slate-200",
+                  `group relative inline-flex h-[8px] w-[32px] flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-lmn focus:ring-offset-2`
                 )}
               >
                 <span
@@ -126,18 +125,19 @@ function ActivityLog({ colorChange, proItem, isAvailable }) {
                 <span
                   aria-hidden="true"
                   className={classNames(
-                    enableDmActivityLog ? "bg-lmn" : "bg-gray-200",
+                    (lmn_admin.simple_history_status === '1' ? enableDmActivityLog : false) ? "bg-lmn" : "bg-gray-200",
                     "pointer-events-none absolute mx-auto h-[16px] w-[32px] rounded-full transition-colors duration-200 ease-in-out"
                   )}
                 />
                 <span
                   aria-hidden="true"
                   className={classNames(
-                    enableDmActivityLog ? "translate-x-5" : "translate-x-0",
+                    (lmn_admin.simple_history_status === '1' ? enableDmActivityLog : false) ? "translate-x-5" : "translate-x-0",
                     "toggle-bubble pointer-events-none absolute left-0 inline-block h-[16px] w-[16px] transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out"
                   )}
                 />
               </Switch>
+              </Tooltip>
             </>
           ) : (
             <button
