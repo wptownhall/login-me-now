d1ab2761cb9c6084f6c2ede4afe9bcc28103531b
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2023-11-13 21:30:52 +0600

    version update 1.3.1

--- login-me-now.php
+++ login-me-now.php
@@ -4,7 +4,7 @@
  * Description: 1 click passwordless login, social login & user switching
  * Author: WPtownhall
  * Author URI: https://wptownhall.com/login-me-now/
- * Version: 1.3.0
+ * Version: 1.3.1
  * Requires PHP: 7.4
  * License: GPL2
  * Text Domain: login-me-now
@@ -45,7 +45,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * This class holds the entire LoginMeNow plugin
  */
 final class LoginMeNow {
-	public string $version  = '1.3.0';
+	public string $version  = '1.3.1';
 	private string $min_php = '7.4';
 	public object $app;
 	private static $instance;
