cca214f2105a26d47d6f079b617b3951905cd651
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-13 08:47:15 +0600

    module text has been updated

--- App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/DashboardModules.js
+++ App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/DashboardModules.js
@@ -126,13 +126,13 @@ function DashboardModules() {
                           <div className="inline-box-flex inline-size-auto">
                             <TickIcon />
                             <span className="ml-3 mb-[12px] text-[#D9F5EC] text-[16px] noto-serif responsive-span">
-                              Express login - WC
+                              Express login - WC & EDD
                             </span>
                           </div>
                           <div className="inline-box-flex inline-size-auto">
                             <TickIcon />
                             <span className="ml-3 mb-[12px] text-[#D9F5EC] text-[16px] noto-serif responsive-span">
-                              Express login - EDD
+                              OTP login
                             </span>
                           </div>
                           <div className="inline-box-flex inline-size-auto">
