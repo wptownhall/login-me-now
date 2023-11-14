db61f09d0173183d74cc742d7e483b36c46ee44f
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-14 13:30:52 +0600

    updated code

--- App/Admin/Assets/src/dashboard-app/pages/license/License.js
+++ App/Admin/Assets/src/dashboard-app/pages/license/License.js
@@ -1,19 +1,20 @@
 import { __ } from '@wordpress/i18n';
 import { useSelector, useDispatch } from 'react-redux';
 import apiFetch from '@wordpress/api-fetch';
+import { useState } from 'react';
 
 const License = () => {
-
+  const [license, setLicense] = useState("")
   let lmnProLic = useSelector((state) => state.lmnProLic);
   const dispatch = useDispatch();
-  const updateLicense = (License) => {
-    dispatch({ type: 'UPDATE_LMN_PRO_LIC', payload: License.target.value });
+  const updateLicense = () => {
+    dispatch({ type: 'UPDATE_LMN_PRO_LIC', payload: license });
     
     const formData = new window.FormData();
     formData.append('action', 'login_me_now_pro_activate_license');
     formData.append('security', lmn_admin.update_nonce);
     formData.append('key', 'lmn_pro_lic');
-    formData.append('value', License.target.value);
+    formData.append('value', license);
 
     apiFetch({
       url: lmn_admin.ajax_url,
@@ -28,13 +29,23 @@ const License = () => {
     });
   };
 
+  console.log(lmnProLic)
+
   return (
     <section className={`text-[16px] block px-8 py-8 justify-between`}>
       <div className='mr-16 w-full flex flex-col space-y-3'>
         <h3 className="p-0 flex-1 justify-right inline-flex text-xl leading-6 font-semibold text-slate-800">
           {__('License', 'login-me-now')}
         </h3>
-        <input onChange={updateLicense} className='block w-full h-[50px] !p-3 !border-slate-200' type='password' name='lmn_pro_lic' value={lmnProLic} placeholder='Enter your license here...' />
+        <div className="flex">
+        <input onChange={(e) => setLicense(e.target.value)} className='block w-full h-[50px] !p-3 !border-slate-200' type='password' name='lmn_pro_lic' value={lmnProLic} placeholder='Enter your license here...' />
+        <button
+          className="h-[50px] !p-3 !border-slate-200 border ml-3 rounded-[4px] text-[#5cabd3] font-semibold"
+          onClick={updateLicense}
+        >
+          Active
+        </button>
+        </div>
         <span class="text-[#2271B1]">
           {__('Add your purchased license here for future updates', 'login-me-now')} 
         </span>
