dc1770d50b0b3767d7f35bd7e76ca78deeb4a80d
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-14 12:25:52 +0600

    failed code

--- App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/modules/License.js
+++ App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/modules/License.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { __ } from "@wordpress/i18n";
 import { useSelector, useDispatch } from "react-redux";
 import apiFetch from "@wordpress/api-fetch";
 
 export default function License() {
-  const [license, setLicense] = useState("");
+  const [license, setLicense] = useState(""); // Initialize with an empty string
   const dispatch = useDispatch();
-  const lmnProLic = useSelector((state) => state.lmnProLic)
+  const lmnProLic = useSelector((state) => state.lmnProLic);
+
+  // Set the initial value of 'license' to 'lmnProLic' when the component mounts
+  useEffect(() => {
+    setLicense(lmnProLic);
+  }, [lmnProLic]);
 
   const updateLicense = () => {
     console.log(license);
@@ -14,8 +19,7 @@ export default function License() {
 
     const formData = new window.FormData();
 
-    formData.append("action", "login_me_now_update_admin_setting");
-    // login_me_now_update_admin_setting
+    formData.append("action", "login_me_now_pro_activate_license");
     formData.append("security", lmn_admin.update_nonce);
     formData.append("key", "lmn_pro_lic");
     formData.append("value", license);
@@ -26,17 +30,18 @@ export default function License() {
       url: lmn_admin.ajax_url,
       method: "POST",
       body: formData,
-    }).then(() => {
-      dispatch({
-        type: "UPDATE_SETTINGS_SAVED_NOTIFICATION",
-        payload: __("Successfully saved!", "login-me-now"),
-      });
+    }).then((data) => {
+      if( false === data.success ) {
+        dispatch({ type: 'UPDATE_SETTINGS_NOT_SAVED_NOTIFICATION', payload: data.data});
+      } else {
+        dispatch({ type: 'UPDATE_SETTINGS_SAVED_NOTIFICATION', payload: __('Successfully saved!', 'login-me-now') });
+      }
     });
   };
 
   return (
     <div className="gap-4 items-start lg:grid-cols-5 lg:gap-0 xl:gap-0 rounded-md bg-white overflow-hidden shadow-sm p-8 mt-8">
-      <h1 className="p-0 flex-1 justify-right inline-flex text-xl leading-6 font-semibold text-slate-800">
+      <h1 className="p-0 flex-1 justify-right inline-flex text-xl leading-6 font-semibold text-slate-800 mb-4">
         Licence
       </h1>
       <div className="flex">
@@ -45,7 +50,7 @@ export default function License() {
           className="block w-full h-[50px] !p-3 !border-slate-200"
           type="password"
           name="lmn_pro_lic"
-          value={license}
+          value={license} 
           placeholder="Enter your license here..."
         />
         <button
