d97aaac183f2afceb75f75e53965acd2fcbea653
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-19 11:54:35 +0600

    frontend design done

--- App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/modules/License.js
+++ App/Admin/Assets/src/dashboard-app/pages/dashboard-modules/modules/License.js
@@ -4,43 +4,55 @@ import { useSelector, useDispatch } from "react-redux";
 import apiFetch from "@wordpress/api-fetch";
 
 export default function License() {
-  const [license, setLicense] = useState(""); 
-  const [licenseSuccessfull , setLicenseSuccessfull] = useState("")
+  const [license, setLicense] = useState("");
+  const [isLicenseActive, setIsLicenseActive] = useState(false);
   const dispatch = useDispatch();
-  const lmnProLic = useSelector((state) => state);
+  const lmnProLic = useSelector((state) => state.lmnProLic);
 
-  // Set the initial value of 'license' to 'lmnProLic' when the component mounts
   useEffect(() => {
     setLicense(lmnProLic);
-  }, [lmnProLic]);
 
-  console.log(license)
+    lmnProLic !== "" && setIsLicenseActive(true);
+  }, [lmnProLic]);
+  console.log(useSelector((state) => state));
+
   const updateLicense = () => {
-    dispatch({ type: "UPDATE_LMN_PRO_LIC", payload: license });
+    dispatch({
+      type: "UPDATE_LMN_PRO_LIC",
+      payload: isLicenseActive === true ? "" : license,
+    });
     const formData = new window.FormData();
 
     formData.append("action", "login_me_now_pro_activate_license");
     formData.append("security", lmn_admin.update_nonce);
     formData.append("key", "lmn_pro_lic");
-    formData.append("value", license);
+    formData.append("value", isLicenseActive === true ? "" : license);
 
     apiFetch({
       url: lmn_admin.ajax_url,
       method: "POST",
       body: formData,
     }).then((data) => {
-      if( false === data.success ) {
-        dispatch({ type: 'UPDATE_SETTINGS_NOT_SAVED_NOTIFICATION', payload: data.data});
+      if (false === data.success) {
+        dispatch({
+          type: "UPDATE_SETTINGS_NOT_SAVED_NOTIFICATION",
+          payload: data.data,
+        });
+        setIsLicenseActive(false);
       } else {
-        dispatch({ type: 'UPDATE_SETTINGS_SAVED_NOTIFICATION', payload: __('Successfully saved!', 'login-me-now') });
+        dispatch({
+          type: "UPDATE_SETTINGS_SAVED_NOTIFICATION",
+          payload: __("Successfully saved!", "login-me-now"),
+        });
+        setIsLicenseActive(true);
       }
 
-      console.log(licenseSuccessfull === "Successfully updated license!" ? true : false);
-      setLicenseSuccessfull(data.data.message);
-      console.log(lmn_admin.lmnProLic)
+      console.log(license);
+      console.log(data);
     });
   };
-  // console.log(lmnProLic)
+
+  console.log("License Active: ", isLicenseActive);
 
   return (
     <div className="gap-4 items-start lg:grid-cols-5 lg:gap-0 xl:gap-0 rounded-md bg-white overflow-hidden shadow-sm p-8 mt-8">
@@ -50,17 +62,26 @@ export default function License() {
       <div className="flex">
         <input
           onChange={(e) => setLicense(e.target.value)}
-          className="block w-full h-[50px] !p-3 !border-slate-200"
+          className={`block w-full h-[50px] !p-3 !border-slate-200 ${
+            isLicenseActive === true ? "placeholder-[#32cb32]" : ""
+          }`}
           type="password"
           name="lmn_pro_lic"
-          value={license}
-          placeholder="Enter your license here..."
+          placeholder={
+            isLicenseActive === true
+              ? "License saved successfully!"
+              : "Enter your license here..."
+          }
         />
         <button
-          className={`h-[50px] !p-3 !border-slate-200 border ml-3 rounded-[4px] ${licenseSuccessfull === "Successfully updated license!" ? 'bg-[#ff0e0e] text-white' : 'text-[#2271B1] bg-white'}`}
+          className={`h-[50px] !p-3 !border-slate-200 border ml-3 rounded-[4px] ${
+            isLicenseActive === true
+              ? " bg-[#d9534f] text-white"
+              : "bg-white text-[#2271B1]"
+          }`}
           onClick={updateLicense}
         >
-          {licenseSuccessfull === "Successfully updated license!" ? 'Deactive' : 'Active'}
+          {isLicenseActive === true ? "Deactive" : "Active"}
         </button>
       </div>
     </div>
