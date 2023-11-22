2765a8d43bce19bcc4311cc3fce6451ae10270d1
muhammadkibria <muhammadmridulhosenkibria@gmail.com>
2023-11-22 13:21:54 +0600

    browser extension video, suggest video are stoped coming and add left side margin

--- App/Admin/Assets/src/dashboard-app/pages/browser-extensions/Video.js
+++ App/Admin/Assets/src/dashboard-app/pages/browser-extensions/Video.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import browserVideoThumbnail from "../../../../images/browser-extension-thumbnail.png"
+import React, { useState } from "react";
+import browserVideoThumbnail from "../../../../images/browser-extension-thumbnail.png";
 
 const Video = ({ width, height }) => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -9,27 +9,27 @@ const Video = ({ width, height }) => {
   };
 
   return (
-    <div>
+    <>
       {!isPlaying ? (
         <img
           src={browserVideoThumbnail}
           alt="Video Thumbnail"
-          style={{ cursor: 'pointer', width, height }}
+          style={{ cursor: "pointer", width, height }}
           onClick={handleThumbnailClick}
         />
       ) : (
         <div>
           <iframe
-            title="YouTube Video"
+            title="Login Me Now Browser Extension : 1 click WordPress admin dashboard login"
             width={width}
             height={height}
-            src={`https://www.youtube.com/embed/2_GOOzZkeSM?autoplay=1&mute=1`}
+            src={`https://www.youtube.com/embed/2_GOOzZkeSM?autoplay=1&mute=1&rel=0`}
             frameBorder="0"
             allowFullScreen
           />
         </div>
       )}
-    </div>
+    </>
   );
 };
 
