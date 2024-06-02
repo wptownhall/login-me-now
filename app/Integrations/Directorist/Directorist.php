e08aae020ad6ec1aa1f73b49692bf46c9389f7b2
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-06-02 22:42:47 +0600

    Add: Directorist Support

--- app/Integrations/Directorist/Directorist.php
+++ app/Integrations/Directorist/Directorist.php
@@ -8,9 +8,15 @@
 namespace LoginMeNow\Integrations\Directorist;
 
 use LoginMeNow\Common\IntegrationBase;
+use LoginMeNow\Providers\LoginFormsServiceProvider;
 
 class Directorist extends IntegrationBase {
 	public function boot(): void {
-		// error_log( print_r('I am directorist boot!' ,true) );
+		add_action( 'atbdp_before_login_form_end', [$this, 'add_form'] );
+		add_action( 'atbdp_before_user_registration_submit', [$this, 'add_form'] );
+	}
+
+	public function add_form() {
+		( new LoginFormsServiceProvider() )->login_buttons( false, true, false );
 	}
 }
